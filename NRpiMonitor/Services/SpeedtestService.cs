77f0711c791629d968cef6382722f63a006727a8
zetroot <zetroot@gmail.com>
2023-02-05 22:12:10 +0300

    expose speedtest

--- NRpiMonitor/Services/SpeedtestService.cs
+++ NRpiMonitor/Services/SpeedtestService.cs
@@ -2,16 +2,21 @@ using System.Diagnostics;
 using System.Text.Json;
 using NRpiMonitor.Database.Repositories;
 using NRpiMonitor.Services.Models.Speedtest;
+using Prometheus;
 
 namespace NRpiMonitor.Services;
 
 public class SpeedtestService
 {
+    private const string Download = nameof(Download);
+    private const string Upload = nameof(Upload);
+    
     private readonly SpeedTestRepository _repo;
+    private static readonly Gauge Speed = Metrics.CreateGauge("bandwidth", "Speedtest results", "direction");
 
     public SpeedtestService(SpeedTestRepository repo)
     {
-        this._repo = repo;
+        _repo = repo;
     }
 
     public async Task RunSpeedtest()
@@ -28,9 +33,16 @@ public class SpeedtestService
         proc.Start();
         var output = await proc.StandardOutput.ReadToEndAsync();
         var result = JsonSerializer.Deserialize<SpeedtestResultJson>(output);
+        ExposeResult(result);
         await _repo.AddResult(result);
     }
-    
+
+    private static void ExposeResult(SpeedtestResultJson result)
+    {
+        Speed.WithLabels(Download).Set(result.Download.Bandwidth);
+        Speed.WithLabels(Upload).Set(result.Upload.Bandwidth);
+    }
+
     public Task<SpeedtestResult> GetLastState() => _repo.GetLastState();
     public Task<List<SpeedtestResult>> GetResults(DateTime notBefore, DateTime notAfter) =>
         _repo.GetAllResults(notBefore, notAfter);

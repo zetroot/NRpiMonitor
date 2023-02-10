9998d7a60cffe8cae19a9bbf4902a2854e2bd0a0
zetroot <zetroot@gmail.com>
2023-02-10 18:56:43 +0300

    Added iperf3 running service

---
+++ NRpiMonitor/Services/iperf/BandwidthService.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using System.Text.Json;
+using NRpiMonitor.Services.iperf.Models;
+using NRpiMonitor.Services.Models.Speedtest;
+using Prometheus;
+
+namespace NRpiMonitor.Services.iperf;
+
+public class BandwidthService
+{
+    private const string Download = nameof(Download);
+    private const string Upload = nameof(Upload);
+    private static readonly Gauge Speed = Metrics.CreateGauge("iperf3_bandwidth", "iperf3 results", "direction"); 
+    private static readonly Gauge Size = Metrics.CreateGauge("iperf3_size", "iperf3 transferred", "direction"); 
+    public async Task RunIperf3()
+    {
+        var proc = new Process()
+        {
+            StartInfo = new ProcessStartInfo()
+            {
+                FileName = "iperf3",
+                Arguments = "-J -c lg.vie.alwyzon.net",
+                RedirectStandardOutput = true
+            }
+        };
+        proc.Start();
+        var output = await proc.StandardOutput.ReadToEndAsync();
+        var result = JsonSerializer.Deserialize<OutputModelRoot>(output);
+        ExposeResult(result);
+    }
+
+    private void ExposeResult(OutputModelRoot? result)
+    {
+        Speed.WithLabels(Download).Set(result?.Summary?.Received?.Bandwidth ?? 0);
+        Speed.WithLabels(Upload).Set(result?.Summary?.Sent?.Bandwidth ?? 0);
+        Size.WithLabels(Download).Set(result?.Summary?.Received?.Size ?? 0);
+        Size.WithLabels(Upload).Set(result?.Summary?.Sent?.Size ?? 0);
+    }
+}

0acf82f382609e0914292542341de92d63ac1d78
zetroot <zetroot@gmail.com>
2023-02-11 11:14:06 +0300

    do not run speedtest with iperf

--- NRpiMonitor/Services/SpeedBackground.cs
+++ NRpiMonitor/Services/SpeedBackground.cs
@@ -1,18 +1,14 @@
-using NRpiMonitor.Services.iperf;
-
 namespace NRpiMonitor.Services;
 
 public class SpeedBackground : BackgroundService
 {
     private readonly SpeedtestService _speedtestService;
-    private readonly BandwidthService _bandwidthService;
     private readonly ILogger<SpeedBackground> _logger;
 
-    public SpeedBackground(SpeedtestService speedtestService, ILogger<SpeedBackground> logger, BandwidthService bandwidthService)
+    public SpeedBackground(SpeedtestService speedtestService, ILogger<SpeedBackground> logger)
     {
         _speedtestService = speedtestService;
         _logger = logger;
-        _bandwidthService = bandwidthService;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -28,14 +24,6 @@ public class SpeedBackground : BackgroundService
                 _logger.LogError(e,"Failed to do speedtest");
             }
             
-            try
-            {
-                await _bandwidthService.RunIperf3();
-            }
-            catch(Exception e)
-            {
-                _logger.LogError(e,"Failed to run iperf3 service");
-            }
             await Task.Delay(TimeSpan.FromMinutes(15), stoppingToken);
         }
     }
